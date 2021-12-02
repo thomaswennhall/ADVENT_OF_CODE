a1bfebf41a15248ee31da9bae6f474e3208b1a56
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2021-12-02 19:45:49 +0100

    Day 2 - part 2

--- Day_2/index.js
+++ Day_2/index.js
@@ -30,3 +30,32 @@ const positionObj = getTotalPerDirection(INPUT)
 const answer = positionObj.depth * positionObj.horizontal
 
 console.log('Part 1: ', answer)
+
+const configWithAim = {
+  up: ['aim', -1],
+  down: ['aim', 1],
+  forward: ['horizontal', 1]
+}
+
+function getTotalWithAim(array) {
+  const position = {
+    depth: 0,
+    horizontal: 0,
+    aim: 0
+  }
+
+  for (const obj of array) {
+    const operatorArr = configWithAim[obj.direction] //=> ['aim': 1]
+    if (operatorArr[0] === 'aim') {
+      position[operatorArr[0]] += operatorArr[1] * obj.value
+    } else {
+      position.horizontal += obj.value
+      position.depth += obj.value * position.aim * operatorArr[1]
+    }
+  }
+  return position
+}
+
+const position2 = getTotalWithAim(INPUT)
+const answer2 = position2.horizontal * position2.depth
+console.log('Part 2: ', answer2)
